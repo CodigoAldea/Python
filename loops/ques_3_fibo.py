11d07236a4a29fe598070bf206321388c9d8bcd0
CodigoAldeam <dmdndrd@gmail.com>
2021-01-06 14:57:38 +0530

    question_for_loop

---
+++ loops/ques_3_fibo.py
@@ -0,0 +1,13 @@
+N = 50
+
+#initialize the list with starting elements: 0, 1
+fibonacciSeries = [1,1]
+
+if N>2:
+	for i in range(2, N):
+		#next elment in series = sum of its previous two numbers
+		nextElement = fibonacciSeries[i-1] + fibonacciSeries[i-2]
+		#append the element to the series
+		fibonacciSeries.append(nextElement)
+
+print(fibonacciSeries)
