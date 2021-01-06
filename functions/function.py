82f1b5c268623680785ac78c4163032956a976ca
CodigoAldeam <dmdndrd@gmail.com>
2021-01-06 16:28:37 +0530

    function

--- functions/function.py
+++ functions/function.py
@@ -5,10 +5,11 @@ def add():
     """
     a, b = 2,3
     c= a+ b
-    print(c)
-    return
+    #print(c)
+    return c
 
-add()
+f= add()
+print(f)
 
 def sum(a , b):
     """
@@ -17,4 +18,39 @@ def sum(a , b):
     sum = a+b
     print(sum)
 
-sum( 2, 5)
+z = 2
+y = 5
+sum(z, y)
+
+
+def printinfo( arg1, *vartuple ):
+   "This prints a variable passed arguments"
+   print ("Output is: ")
+   print (arg1)
+   for var in vartuple:
+      print (var)
+   return
+
+printinfo( 10 , 20, 50 )
+printinfo( 10 )
+
+
+#sum of 3 numbers 
+#multiply by 5 
+
+def sum_of():
+    a, b, c = 2, 3 ,5
+    d= a+b+c
+    return d
+
+x = sum_of()
+z = x*5
+print (z)
+
+
+# var = lambda [arg1 [,arg2,.....argn]]:expression
+
+
+mul = lambda a , b: a*b
+
+print (mul(3,5))
