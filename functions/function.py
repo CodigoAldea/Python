a051dd790041bf798ce2ac0f23ff9ea180602734
CodigoAldeam <dmdndrd@gmail.com>
2021-01-06 15:54:59 +0530

    functions

---
+++ functions/function.py
@@ -0,0 +1,20 @@
+# adding two numbers 
+def add():
+    """
+    adding two numbers 
+    """
+    a, b = 2,3
+    c= a+ b
+    print(c)
+    return
+
+add()
+
+def sum(a , b):
+    """
+    parametarised function for sum of two number
+    """
+    sum = a+b
+    print(sum)
+
+sum( 2, 5)
---
+++ loops/while.py
@@ -0,0 +1,6 @@
+#count
+count = 0
+while (count < 9):
+   print ('The count is:', count)
+   count = count + 1
+   
