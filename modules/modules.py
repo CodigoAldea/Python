06c751168b8721f74e76dbc90a05a8d23500f1e0
CodigoAldeam <dmdndrd@gmail.com>
2021-01-11 16:01:06 +0530

    new

--- modules/modules.py
+++ modules/modules.py
@@ -1,38 +1,8 @@
-#output
+dict_1 = {'One':1, 'Two':2}
+dict_2 = {'Two':2, 'Three':3}
+dictionary = {**dict_1, **dict_2}
+print(dictionary)
 
-
-a = 20
-
-# use formatted string literals (f-string)
-# f or F 'string'
-# variable is inside {}
-print(f'the number is {10}')
-
-# str.format()
-
-b = 30 
-
-print('the are ' + '{} ,{}'.format(b , 10) + ' people')
-
-# str() : output : human redable form
-# repr() : output : interpreter redable format
-
-# can we print or give output a LIST as a string ? 
-
-f = [2 ,2 ,4 ,5]
-
-for i in range(len(f)):
- print(f'this is a list {f[i]}')
-
-print(f'this is a list {f}')
-
-# number {} = number of arg
-# more than two multiplication table : 
-
-a = 3 
-b = 4
-# range - 1 to 10, for loop
-for i in range(1,11):
-    #print('{} and {}'.format(a*i,b*i))
-    print(str(a*i), repr(b*i))
-    #print(repr(b*i))
+list_1 = ["One","Two","Three"]
+list_2 = [1,2,3]
+dictionary1 = dict(zip(list_1, list_2))
