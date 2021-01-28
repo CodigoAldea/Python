0255501f970ad7350f1a99be77d678b158e17626
CodigoAldeam <dmdndrd@gmail.com>
2021-01-28 16:03:51 +0530

    linear

---
+++ Questions_practice/Sorting/Linear_or_seq_search.py
@@ -0,0 +1,21 @@
+''' Sequential Search '''
+''' while conditions : 
+    
+    while pos < len(dlist) and not found:
+        
+        where 
+
+        dlist is the list 
+
+        pos = first index
+
+        found is counter
+'''
+
+l = [2,3,4,10,40]
+v = 4
+
+for i in range(6):
+    if l[i] == v :
+        print('fount at', i)
+        
