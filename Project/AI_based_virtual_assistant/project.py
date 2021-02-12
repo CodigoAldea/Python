062fb44cdd3a7536736c0c654e12429159c89023
CodigoAldeam <dmdndrd@gmail.com>
2021-02-12 11:56:40 +0530

    pro

---
+++ Project/AI_based_virtual_assistant/project.py
@@ -0,0 +1,12 @@
+import speech_recognition as sr
+
+listener = sr.Recognizer()
+
+try:
+    with sr.Microphone() as source:
+        print('listning...')
+        voice = listener.listen(source)
+        command = listener.recognize_google(voice)
+        print(command)
+except:
+    pass
