23948b3d5893d24d151e9dea39c164c480ee42cf
CodigoAldeam <dmdndrd@gmail.com>
2021-02-12 12:45:42 +0530

    pro

--- Project/AI_based_virtual_assistant/project.py
+++ Project/AI_based_virtual_assistant/project.py
@@ -1,12 +1,40 @@
 import speech_recognition as sr
+import pyttsx3
+import pywhatkit
 
 listener = sr.Recognizer()
+engine = pyttsx3.init()
+# voice chagne
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
 
-try:
-    with sr.Microphone() as source:
-        print('listning...')
-        voice = listener.listen(source)
-        command = listener.recognize_google(voice)
-        print(command)
-except:
-    pass
+
+def take_command():
+    try:
+        with sr.Microphone() as source: # to create a microphone instance 
+            print('listning...')
+            voice = listener.listen(source) # to listen 
+            command = listener.recognize_google(voice) # speach to text
+            #if 'tom' in command:
+            print(command)
+    except:
+        pass
+    return command
+
+def run_pro():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing' + song)
+        pywhatkit.playonyt(song)
+    if 'what is' in command:
+        text = command.replace('what is', '')
+        talk('searching for' + text)
+        pywhatkit.search(text)
+
+
+run_pro()
