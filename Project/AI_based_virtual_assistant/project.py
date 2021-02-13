2b0fe237967c1c2604a7a8a128e230ef8ee39996
CodigoAldeam <dmdndrd@gmail.com>
2021-02-13 11:47:25 +0530

    alexa

--- Project/AI_based_virtual_assistant/project.py
+++ Project/AI_based_virtual_assistant/project.py
@@ -1,17 +1,22 @@
-import speech_recognition as sr
-import pyttsx3
-import pywhatkit
+import speech_recognition as sr # speech to text
+import pyttsx3 # text to speech
+import pywhatkit # third party app
+import datetime # to check the date an time.
+import wikipedia # for searching wikipedia
+import pyjokes # for joke
 
 listener = sr.Recognizer()
 engine = pyttsx3.init()
-# voice chagne
+# voice change
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
+
+# function to output voice 
 def talk(text):
     engine.say(text)
     engine.runAndWait()
 
-
+# read the command provided
 def take_command():
     try:
         with sr.Microphone() as source: # to create a microphone instance 
@@ -19,22 +24,37 @@ def take_command():
             voice = listener.listen(source) # to listen 
             command = listener.recognize_google(voice) # speach to text
             #if 'tom' in command:
-            print(command)
+            #print(command)
     except:
         pass
     return command
 
 def run_pro():
     command = take_command()
-    print(command)
+    # print(command)
+
+    # play the song 
     if 'play' in command:
         song = command.replace('play', '')
         talk('playing' + song)
         pywhatkit.playonyt(song)
-    if 'what is' in command:
-        text = command.replace('what is', '')
-        talk('searching for' + text)
-        pywhatkit.search(text)
+    # time
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %P') # %I for 12 hour time formate and %p for pm and am
+        talk('the time is ' + time)
+        print('the time is'+ time)
+    # search the web 
+    elif 'what is ' in command:
+        item = command.replace('what is', '')
+        info = wikipedia.summary(item, 5)
+        talk(info)
+        print(info)
+    # to get jokes
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+    # when the condition is not fulfilled
+    else :
+        talk(" didn't got that, please repeat")
 
-
-run_pro()
+while True:
+    run_pro()
