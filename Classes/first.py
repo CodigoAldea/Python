b91ae0ce7207f4818de1f3d5910bec632490db45
Codigo Aldea <65016894+CodigoAldea@users.noreply.github.com>
2021-09-24 12:36:08 +0530

    list

---
+++ Classes/first.py
@@ -0,0 +1,3 @@
+t =('p','y','t','h','o','n')
+print ("fourth element of the tuple : ", t[3])
+print ("fourth element from the last : ", t[-4])
--- Tutorials/Lists/ques_3.py
+++ Tutorials/Lists/ques_3.py
@@ -3,7 +3,9 @@ removing the 0th, 4th and 5th elements.'''
 
 a = [1,2,3,4,5,6,7,8]
 b = [0,4,5]
+c=[]
 for i in range(len(a)):
-    if i in b:
-        a.remove(a[i])
-print(a)
+    if i not in b:
+        c.append(a[i])
+        print(i)
+print(c)
