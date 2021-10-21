6163a9797c313f2b9daacee27fc04fe2c0911eb6
Codigo Aldea <65016894+CodigoAldea@users.noreply.github.com>
2021-10-21 15:38:25 +0530

    new

---
+++ Neeraj/datatype.py
@@ -0,0 +1,24 @@
+'''
+text : string
+numeric : int, float, complex,
+squence : list, tuple, range
+mapping : dict
+set : set , frozenset
+boolean : bool
+binary : bytes, bytearray, memoryview 
+
+v = "abcd"
+i = 6 
+f = 4.5
+c = 4j
+l = []
+t = ()
+r = range(10)
+d = {"key" : "value", } 
+s = {}
+fs = frozenset({})
+b = True or False
+bt = b"hello"
+ba = bytearray(6)
+mv = memoryview(bytes(5)) '''
+
---
+++ Neeraj/intro.py
@@ -0,0 +1,10 @@
+
+def type():
+    a = 5 
+    b = 6
+    return a+b
+def type1(a,b) :
+    return a*b
+a = type()
+b = type1(a, 5)
+print(b)
