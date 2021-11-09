620753757a679f07fe807bc760520237e3c3a916
Codigo Aldea <65016894+CodigoAldea@users.noreply.github.com>
2021-11-09 14:54:11 +0530

    new

---
+++ Neeraj/string/string.py
@@ -0,0 +1,34 @@
+'''a = "python"
+# 1st : using range 
+for i in range(len(a)):
+    print(a[i])
+
+print('-----------')
+# 2nd : using the variable
+for i in a:
+    print(i)
+
+s = "this is a python tutorial"
+print ("python" not in s)
+
+# slicing 
+s = "python"
+
+print(s[::-1])
+#print(s[3:])'''
+
+
+'''
+s = 'python'
+print(s)
+
+# reading string and printing 
+# index 
+for i in range(len(s)):
+    print(s[i])
+# characters 
+for i in s: 
+    print(i)
+
+print(s[0::2])
+'''
