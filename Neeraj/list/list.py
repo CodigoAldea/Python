3c0f7136bb6e6ead48a95d2cdba5efb9e7285226
Codigo Aldea <65016894+CodigoAldea@users.noreply.github.com>
2021-11-11 14:08:11 +0530

    new

---
+++ Neeraj/list/list.py
@@ -0,0 +1,27 @@
+a = ['a',1,2.3,True,[],(),{}]
+
+# reading for loop 1: index 2 : characters 
+# len() for lenthe of the list 
+# mutable 
+# accessing is done by index (positive and negative)
+# range function for indexing. 
+# slicing start, stop and step.
+# change of item insert()
+#a.insert(2, 100)
+# append
+#a.append('comp')
+# extend it merg 2 list 
+#b = ['python', '55', 33,55.6]
+#a.extend(b)
+#remove character
+#a.remove(2.3)
+# pop index and default value -1 or last index.
+# del used to delete the list or the value on a specific  index
+# clear this is use to empty the list
+#a.clear()
+# sort function is used to sor the list (default : ascending)
+# To sort descending, use the keyword argument reverse = True
+#a.sort(reverse = True)
+# count 
+# x = a.count(4)
+print (a)
